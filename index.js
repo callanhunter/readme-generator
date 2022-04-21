4e96ac3accf97122dda4ea9e9c69318f370d3f80
callanhunter <callan57hunter@gmail.com>
2022-04-21 08:53:17 -0700

    add question prompt

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ {
+    message: "What is the title of your project?",
+    type: "input",
+    name: "title",
+},
+{
+    message: "Write a description about your project?",
+    type: "input",
+    name: "description",
+},
+{
+    message: "What instillation is required for users to use your application?",
+    type: "input",
+    name: "instillation",
+},
+{
+    message: "What is the license for this project?",
+    type: "list",
+    name: "license",
+    choices: []
+},
+{
+    message: "Are there any other contributors on the project?",
+    type: "input",
+    name: "contributors",
+},
+{
+    message: "table of contents",
+    type: "input",
+    name: "table of contents",
+},
+{
+    message: "usage?",
+    type: "input",
+    name: "usage",
+},
+{
+    message: "tests?",
+    type: "input",
+    name: "tests",
+},
+{
+    message: "What is your Github username?",
+    type: "input",
+    name: "questions",
+},
+{
+    message: "What is your Email Adress?",
+    type: "input",
+    name: "questions",
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
